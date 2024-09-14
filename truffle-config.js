067e1014b9d7f981db5d6dc17af8efc878d7b962
pavan kalyan <p.kalyanvarikolu@gmail.com>
2024-09-14 16:31:25 +0100

    made some changes in truffle file

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,42 @@
+require('dotenv').config();
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ganache port (default: none)
-      network_id: "*",       // Any network (default: none)
+    sepolia: {
+      provider: () => new HDWalletProvider(
+        process.env.PRIVATE_KEY,
+        `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+      ),
+      network_id: 11155111,  // Sepolia's network id
+      gas: 5500000,          // Gas limit
+      confirmations: 2,      // # of confirmations to wait between deployments
+      timeoutBlocks: 200,    // # of blocks before a deployment times out
+      skipDryRun: true       // Skip dry run before migrations
     },
+    mainnet: {
+      provider: () => new HDWalletProvider(
+        process.env.PRIVATE_KEY,
+        `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+      ),
+      network_id: 1,         // Mainnet's network id
+      gas: 5500000,          // Gas limit
+      gasPrice: 20000000000, // 20 gwei
+      confirmations: 2,      // # of confirmations to wait between deployments
+      timeoutBlocks: 200,    // # of blocks before a deployment times out
+      skipDryRun: false      // Do not skip dry run before migrations
+    }
   },
   compilers: {
     solc: {
-      version: "0.8.0",      // Fetch exact version from solc-bin
-    },
-  },
+      version: "0.8.0",      // Solidity compiler version
+      settings: {
+        optimizer: {
+          enabled: true,    // Enable optimizer
+          runs: 200         // Optimization runs
+        }
+      }
+    }
+  }
 };
